b40335da481dc5518caf7cd0d7ce114c62b15444
morganbronson <morgan.bronson@ocsbstudent.ca>
2020-12-05 03:10:46 +0000

    move text

--- js/script.js
+++ js/script.js
@@ -9,3 +9,8 @@ function changered () { // eslint-disable-line no-unused-vars
   document.getElementById('mytext').style.color = 'red'
   document.getElementById('mytext2').style.color = 'red'
 }
+
+function movetext () { // eslint-disable-line no-unused-vars
+  document.getElementById('mytext').style.marginLeft = '50px'
+  document.getElementById('mytext2').style.marginLeft = '50px';
+}
